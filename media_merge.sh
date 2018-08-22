4ba3a0689f77c9bd194395496d7a857cdfacf5e6
Ameer Dawood <ameer1234567890@gmail.com>
2018-08-22 17:39:55 +0500

    Modernize

--- media_merge.sh
+++ media_merge.sh
@@ -1,27 +1,30 @@
+#!/bin/sh
 video_folder="video"
 audio_folder="audio"
 output_folder="output"
 mkdir -p "$output_folder"
 
-files="`ls \"$video_folder\"/*.{mp4,mpg,mpeg,MP4,MPG,MPEG} 2> /dev/null`"
+file_exts="mp4 mpg mpeg MP4 MPG MPEG"
+for ext in $file_exts; do
+  files="$files $(ls $video_folder/*."$ext" 2> /dev/null)"
+done
 if [ ! "$files" ]; then
   echo "No video files found! Exiting...."
   exit 1
 fi
 
-files_count="`echo \"$files\" | wc -l`"
 i=0
 while read -r file; do
-  i="`expr $i + 1`"
-  file="`echo \"$file\" | cut -d '/' -f 2`"
-  echo -n "Processing file $i...."
+  i="$((i + 1))"
+  file="$(echo "$file" | cut -d '/' -f 2)"
+  printf "Processing file %s...." "$i"
   video_file="$file"
-  audio_file="`echo \"$file\" | sed -e 's/MP4 [0-9]*p/MP4 Audio/g'`"
-  if [ ! "$audio_folder/$audio_file" ]; then
+  audio_file="$(echo "$file" | sed -e 's/MP4 [0-9]*p/MP4 Audio/g')"
+  if [ ! -f "$audio_folder/$audio_file" ]; then
     echo "Error: Audio file not found! Skipping file...."
   else
     echo "OK"
-    echo -n "Merging file $i...."
+    printf "Merging file %s..." "$i"
     ffmpeg -hide_banner -loglevel panic -i "$video_folder/$video_file" -i "$audio_folder/$audio_file" -c copy "$output_folder/$video_file"
 
     ffmpeg_status="$?"
@@ -36,7 +39,7 @@ while read -r file; do
     fi
 
     echo "Done!"
-    echo -n "Cleaning up source files...."
+    printf "Cleaning up source files...."
     rm "$video_folder/$video_file"
     rm "$audio_folder/$audio_file"
     echo "Done!"
